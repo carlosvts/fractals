9175a26ca3b69ae3088d1ee2a8bbc7b555804af1
carlosvts <carlosvtsdev@gmail.com>
2026-02-28 01:19:24 -0300

    add camera control and switch color to red only

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ void DrawBranch(float xpos, float ypos, float lenght, float angle, float thickne
     float y_end = ypos - cosf(angle) * lenght;
     Vector2 end = {x_end, y_end};
     std::uniform_int_distribution<std::size_t> randomcolor(0, colors.size() - 1);
-    DrawLineEx(start, end, thickness, colors[randomcolor(mtgen)]);
+    DrawLineEx(start, end, thickness, MAROON);
     
     float new_lenght = lenght * 0.70;
     float new_right_angle = angle + 15*DEG2RAD;
@@ -32,13 +32,44 @@ void DrawBranch(float xpos, float ypos, float lenght, float angle, float thickne
 int main(void)
 {
     InitWindow(WIDTH, HEIGHT, "fractal");
-    SetTargetFPS(1);
+    SetTargetFPS(24);
+    
+    // Camera
+    Camera2D camera = { 0 };
+    camera.target = (Vector2) {WIDTH/2.0f, HEIGHT/2.0f};
+    camera.offset = (Vector2) {WIDTH/2.0f, HEIGHT/2.0f};
+    camera.zoom = 1.0f;
+    camera.rotation = 0.0f;
+
+    Camera2D default_camera = camera; 
 
     while (!WindowShouldClose())
     {
+        // camera targets mouse 
+        float mousex = GetMouseX();
+        float mousey = GetMouseY();
+        
+        if (GetMouseWheelMove())
+        {
+            Vector2 mousexy = GetScreenToWorld2D(GetMousePosition(), camera);
+            // Camera zoom controls
+            // Uses log scaling to provide consistent zoom speed
+            camera.zoom = expf(logf(camera.zoom) + ((float)GetMouseWheelMove()*0.1f));
+            
+            Vector2 mousexy_after = GetScreenToWorld2D(GetMousePosition(), camera);
+
+            camera.target.x += mousexy.x - mousexy_after.x;
+            camera.target.y += mousexy.y - mousexy_after.y;
+        }
+        if (IsKeyPressed(KEY_R))
+        {
+            camera = default_camera;
+        }
         BeginDrawing();
             ClearBackground(BLACK);
-            DrawBranch(WIDTH/2, HEIGHT - 20, 300, 0, 20);             
+            BeginMode2D(camera);
+                DrawBranch(WIDTH/2, HEIGHT - 20, 300, 0, 20);             
+            EndMode2D();
         EndDrawing();
     }
 
